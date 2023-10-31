8a20e6bdc32be49fb2d5affa3f7e2513d8ef1338
Saurabh <ssuryavanshi@ineuron.ai>
2023-10-31 18:42:44 +0530

    added show online people feature

--- index.js
+++ index.js
@@ -9,20 +9,45 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+const onlineUsernames = {};
+
 io.on("connection", (socket) => {
-  console.log("A User Connected");
+  console.log("A User Connected", socket.client.id);
 
   socket.on("chat message", (msg) => {
     socket.broadcast.emit("chat message", msg);
   });
 
   socket.on("user connection", (username) => {
-    console.log("user connection notification server", username);
-    io.emit("user connection", username);
+    onlineUsernames[username] = socket.client.id;
+    console.log(onlineUsernames);
+    io.emit("user connection", {
+      username,
+      onlineUsernames,
+    });
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    const disconnectedClientId = socket.client.id;
+    console.log("User disconnected", socket.client.id);
+
+    const disconnectedUsername = Object.keys(onlineUsernames).find(
+      (username) => onlineUsernames[username] === disconnectedClientId
+    );
+
+    console.log(disconnectedUsername);
+
+    delete onlineUsernames[disconnectedUsername];
+
+    socket.broadcast.emit("disconnection", disconnectedUsername);
+  });
+
+  socket.on("typing notification on", (username) => {
+    socket.broadcast.emit("typing notification on", username);
+  });
+
+  socket.on("typing notification off", (username) => {
+    socket.broadcast.emit("typing notification off", username);
   });
 });
 
